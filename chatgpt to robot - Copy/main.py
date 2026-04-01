3cd1799c06b464b85d0619d39720466811f5d5fb
syeda sakina shahzadi bukhari <sakina0001w@gmail.com>
2026-04-01 20:20:08 +0500

    Initial commit

---
+++ chatgpt to robot - Copy/main.py
@@ -0,0 +1,11 @@
+from openai import OpenAI
+import os
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": "Hello"}]
+)
+
+print(response.choices[0].message.content)
